8671b04208dbf856eff17b4bd5cc3ce678e0acc4
NIKHIL GEMBALI <gembalinikhil78@gmail.com>
2026-06-22 22:24:57 +0530

    Update app.py

--- Task4_Sales_Prediction/app.py
+++ Task4_Sales_Prediction/app.py
@@ -14,7 +14,14 @@ st.set_page_config(
 # =====================================
 # Load Trained Model
 # =====================================
-with open("sales_model.pkl", "rb") as file:
+import os
+import pickle
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "sales_model.pkl")
+
+with open(model_path, "rb") as file:
     model = pickle.load(file)
 
 # =====================================
